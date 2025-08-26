ba2fc372c8e989b78ad39dc5890d8315be282115
Pariskrit12 <bhagataryan784@gmail.com>
2025-08-26 21:52:33 +0545

    Written logic for login, logout and regenerate Token

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -3,6 +3,26 @@ import { ApiError } from "../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import { sendWelcomeMail } from "../service/sendWelcomeMail.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import jwt from "jsonwebtoken";
+const generateAccessAndRefreshToken = async function (userId) {
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      throw new ApiError(404, "User not Found");
+    }
+    const refreshToken = user.generateRefreshToken();
+    const accessToken = user.generateAccessToken();
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "Something went wrong while generating access and refresh token"
+    );
+  }
+};
 const userRegister = asyncHandler(async (req, res) => {
   const { username, email, password } = req.body;
   if (!username || !email || !password) {
@@ -39,4 +59,122 @@ const userRegister = asyncHandler(async (req, res) => {
     .status(200)
     .json(new ApiResponse(200, createdUser, "UserCreatedSuccessfully"));
 });
-export { userRegister };
+
+const loginUser = asyncHandler(async (req, res) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    throw new ApiError(400, "All Fields are required");
+  }
+
+  const userExist = await User.findOne({ username });
+  if (!userExist) {
+    res.status(400).json({
+      field: "username",
+      message: "Username doesnot exist",
+    });
+  }
+
+  const isMatch = await userExist.comparePassword(password);
+  if (!isMatch) {
+    res.status(400).json({
+      field: "password",
+      message: "Password is wrong",
+    });
+  }
+
+  const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
+    userExist?._id
+  );
+
+  const loggedIn = await User.findById(userExist._id).select(
+    "-password -refreshToken"
+  );
+  const option = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  res
+    .status(201)
+    .cookie("accessToken", accessToken, option)
+    .cookie("refreshToken", refreshToken, option)
+    .json(
+      new ApiResponse(
+        201,
+        { user: loggedIn, accessToken, refreshToken },
+        "User logged in Successfully"
+      )
+    );
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        refreshToken: undefined,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+
+  const option = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  res
+    .status(200)
+    .clearCookie("accessToken", option)
+    .clearCookie("refreshToken", option)
+    .json(new ApiResponse(200, "User logged out successfully"));
+});
+
+const refreshTokenRegenerate = asyncHandler(async (req, res) => {
+  const incomingRefreshToken = req.cookies?.refreshToken;
+
+  if (!incomingRefreshToken) {
+    throw new ApiError(401, "Invalid authorization");
+  }
+
+  try {
+    const decodedToken = jwt.verify(
+      incomingRefreshToken,
+      process.env.REFRESH_TOKEN_SECRET
+    );
+    const user = await User.findById(decodedToken?._id);
+    if (!user) {
+      throw new ApiError(401, "Invalid refresh token");
+    }
+
+    if (incomingRefreshToken !== user.refreshToken) {
+      throw new ApiError(401, "Refresh token didnot matched");
+    }
+
+    const option = {
+      httpOnly: true,
+      secure: true,
+    };
+
+    const { accessToken, newRefreshToken } =
+      await generateAccessAndRefreshToken(user?._id);
+    return res
+      .status(200)
+      .cookie("accessToken", accessToken, option)
+      .cookie("refreshToken", newRefreshToken, option)
+      .json(
+        200,
+        { accessToken, newRefreshToken },
+        "Access Token refreshed Successfully"
+      );
+  } catch (error) {}
+});
+
+const sendMessage = asyncHandler(async (req, res) => {
+  res.status(200).json(new ApiResponse(200, "yooooooooo"));
+});
+
+export { userRegister, loginUser, sendMessage, logoutUser };
